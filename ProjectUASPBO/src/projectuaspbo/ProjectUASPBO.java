0a1eb5cbb1ad6d00521ee91b3f3fdd4f7b19484f
NurFajriF <90152275+NurFajriF@users.noreply.github.com>
2023-01-07 20:05:50 +0700

    Add files via upload
    Project UAS PBO

---
+++ ProjectUASPBO/src/projectuaspbo/ProjectUASPBO.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectuaspbo;
+
+import pesanan.Pesanan;
+
+/**
+ *
+ * @author Asus
+ */
+public class ProjectUASPBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //untuk menampilkan menu dan memilih dari input Scanner
+        Pesanan pesan1 = new Pesanan();
+        //pesan1.printMenu();
+        
+        
+        /*untuk menampilkan langsung rincian pesanan pada soal UAS
+        -	Roti Manis (varian 1)	: 50 pcs
+        -	Roti Manis (varian 2)	: 100 pcs
+        -	Roti Manis (varian 3)	: 70 pcs
+        -	Roti Tawar (varian 1)	: 20 pcs
+        -	Roti Tawar (varian 2)	: 15 pcs
+        -	Pizza (varian 1)        : 10 pcs
+        */
+        Pesanan pesan2 = new Pesanan();
+//        
+//        pesan2.pesanManis1(50);
+//        pesan2.pesanManis2(100);
+//        pesan2.pesanManis3(70);
+//        
+//        pesan2.pesanTawar1(20);
+//        pesan2.pesanTawar2(15);
+//        
+//        pesan2.pesanPizza1(10);
+    }
+    
+}
